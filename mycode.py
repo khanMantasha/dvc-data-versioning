ed0a8da0fea22708a2d1dbfa3c9b0a049b85ab0b
Mantasha Khan <khanmantasha67890@gmail.com>
2026-08-25 15:57:27 +0530

    Version 2 of data

--- mycode.py
+++ mycode.py
@@ -4,12 +4,10 @@ import csv
 os.makedirs("data", exist_ok=True)
 
 file_path = "data/data.csv"
-
-with open(file_path, "w", newline="") as file:
+with open(file_path, "a", newline="") as file:
     writer = csv.writer(file)
 
-    writer.writerow(["id", "name", "score"])
-    writer.writerow([1, "Alice", 90])
-    writer.writerow([2, "Bob", 85])
+    writer.writerow([3, "Charlie", 95])
 
+print("New row added successfully!")
 print("CSV file created successfully!")
